4cfb37611f363a56d26a282965e58434f5154a41
akos-n <akos.nagy95@gmail.com>
2020-10-25 19:04:18 +0100

    delete console logs

--- src/_services/ServiceUtils.js
+++ src/_services/ServiceUtils.js
@@ -1,6 +1,4 @@
 function getServerURL() {
-  console.log(process.env);
-
   return process.env.REACT_APP_EASY_LEARNING_URL !== undefined
     ? process.env.REACT_APP_EASY_LEARNING_URL
     : "http://localhost:3500/";
